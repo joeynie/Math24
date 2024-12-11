f1d43a8e401297e2521852e180dd3ea026f2c917
joeynie <2974733534@qq.com>
2024-12-11 15:49:57 +0800

    delete build folder

--- build/fltk-1.4.0rc3/fluid/CMakeFiles/fluid-lib.dir/mergeback.cxx.o.d
+++
@@ -1,3 +0,0 @@
-fltk-1.4.0rc3/fluid/CMakeFiles/fluid-lib.dir/mergeback.cxx.o: \
- /home/joe/Desktop/Workspace/Math24/fltk-1.4.0rc3/fluid/mergeback.cxx \
- /usr/include/stdc-predef.h
--- build/fltk-1.4.0rc3/src/CMakeFiles/fltk.dir/xutf8/is_spacing.c.o.d
+++
@@ -1,5 +0,0 @@
-fltk-1.4.0rc3/src/CMakeFiles/fltk.dir/xutf8/is_spacing.c.o: \
- /home/joe/Desktop/Workspace/Math24/fltk-1.4.0rc3/src/xutf8/is_spacing.c \
- /usr/include/stdc-predef.h \
- /home/joe/Desktop/Workspace/Math24/fltk-1.4.0rc3/src/xutf8/../utf8_internal.h \
- /home/joe/Desktop/Workspace/Math24/fltk-1.4.0rc3/src/xutf8/headers/spacing.h
